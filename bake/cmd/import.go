ea82be302d15b70cfaeaf077c91e86d44f900136
John Barker <jebarker@gmail.com>
2019-02-24 16:41:31 -0700

    Switch slug and provider argument around

--- bake/cmd/import.go
+++ bake/cmd/import.go
@@ -11,15 +11,15 @@ import (
 
 // importCmd represents the import command
 var importCmd = &cobra.Command{
-	Use:   "import [provider] [slug] [channel_url]",
+	Use:   "import [slug] [provider] [channel_url]",
 	Short: "Import a Channel into BreadtubeTV",
 	Long: fmt.Sprintf(`Add the supplied channel into BreadtubeTV, without having to edit JSON.
 	
 	Available providers: %s`, strings.Join(ProviderNames(), ", ")),
 	Args: cobra.ExactArgs(3),
 	Run: func(cmd *cobra.Command, args []string) {
-		var provider = args[0]
-		var slug = args[1]
+		var slug = args[0]
+		var provider = args[1]
 		var channelURL, err = util.ParseURL(args[2])
 
 		if err != nil {
