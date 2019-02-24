2511bb034fbe57fc2aaf70710b8f237aad178246
John Barker <jebarker@gmail.com>
2019-02-24 16:00:08 -0700

    Update youtube test to include slug

--- bake/providers/youtube_test.go
+++ bake/providers/youtube_test.go
@@ -8,7 +8,8 @@ import (
 )
 
 func TestFormatChannelDetails(t *testing.T) {
-	channel, err := formatChannelDetails(util.MustParseURL("https://www.youtube.com/channel/UC2-i3KuYoODXsM99Z3-Gm0A"))
+	channel, err := formatChannelDetails("Friendly-Jordies", util.MustParseURL("https://www.youtube.com/channel/UC2-i3KuYoODXsM99Z3-Gm0A"))
 	assert.NoError(t, err)
 	assert.Equal(t, "friendlyjordies", channel.Name)
+	assert.Equal(t, "Friendly-Jordies", channel.Slug)
 }
