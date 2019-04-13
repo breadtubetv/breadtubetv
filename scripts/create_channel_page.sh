35d1d70ab0f15402f16b4b7c27ee17a3c60f421f
Dirk Kelly <github@dirkkelly.com>
2019-04-13 10:24:22 +0800

    permalink if page doesn't exist

--- scripts/create_channel_page.sh
+++ scripts/create_channel_page.sh
@@ -2,7 +2,6 @@ ls data/channels/ | while read data; do
   name=`sed -n -e 's/^name: //p' data/channels/${data}`;
   slug=`sed -n -e 's/^slug: //p' data/channels/${data}`;
   page="content/${data%.yml}.md";
-  echo "permalink: $slug" >> "data/channels/${data}";
   if [ ! -e "$page" ]; then
     echo "---
 title: $name
@@ -13,5 +12,6 @@ menu:
     parent: "Channels"
 videos:
 ---" >> "$page";
+    echo "permalink: $slug" >> "data/channels/${data}";
   fi
 done
