3d87445bf4f430fc21d8d9df767f12057b9e6859
Craig Lonsdale <craig.lonsdale@gmail.com>
2019-07-26 20:07:07 +0800

    Testing changes

---
+++ functions/twitch.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "!!Hello, " + cc.Client.AppTitle,
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
