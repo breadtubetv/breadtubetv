f5b1e91d6f0a5d5ff4cc6b7c2555493ad58f6ed2
Dirk Kelly <github@dirkkelly.com>
2019-09-03 15:56:21 +0800

    Script Fixes

--- scripts/02_move_channel_page_to_bundle.sh
+++ scripts/02_move_channel_page_to_bundle.sh
@@ -1,13 +1,12 @@
 ls content/channels/ | while read data; do
-  if [ $data != "_index.md" ]
+  if [[ $data != *"_"* ]]
   then
-    page="content/channel/${data}";
-    slug=`sed -n -e 's/^channel: //p' $page`;
-    slug=`echo $slug | tr -d '"'`;
+    page="content/channels/${data}";
+    slug=`echo $data | sed -e "s/.md$//";`;
     echo $slug;
 
-    mkdir content/channels/$slug
-    mv $page content/channels/$slug/_index.md
-    mv static/img/channels/$slug.jpg content/channels/$slug/logo.jpg
+    mkdir content/channels/$slug;
+    mv $page content/channels/$slug/index.md;
+    mv static/img/channels/$slug.jpg content/channels/$slug/logo.jpg;
   fi
 done
