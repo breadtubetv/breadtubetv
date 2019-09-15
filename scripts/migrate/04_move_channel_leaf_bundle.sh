978cda7654da0774667a0b18ab73da18fa686c7e
Dirk Kelly <github@dirkkelly.com>
2019-09-15 10:30:35 +0800

    scripts/migrate.sh

--- scripts/migrate/04_move_channel_leaf_bundle.sh
+++ scripts/migrate/04_move_channel_leaf_bundle.sh
@@ -1,6 +1,5 @@
 ls content/channels/*/index.md | while read page; do
- id=$(echo "$page" | sed 's/\/\index.md//g');
- id=$(echo "$id" | sed 's/content\/channels\///g');
+ id=$(echo "$page" | sed 's/\/\index.md//g' | sed 's/content\/channels\///g');
 
  echo $id;
  mv content/channels/$id/index.md content/channels/$id/_index.md;
