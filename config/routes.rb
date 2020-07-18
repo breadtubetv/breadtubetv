4d57fea0f1a211ce589b3b628d6c12f9cb79836b
Dirk Kelly <github@dirkkelly.com>
2020-07-18 13:02:19 +0800

    force ssl, redirect home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/", to: redirect("/channels")
   resources :posts
   resources :channels, param: :slug
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
