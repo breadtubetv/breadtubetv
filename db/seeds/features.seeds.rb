e3629001371c0d88e533c9426153d53e1ca58687
Dirk Kelly <github@dirkkelly.com>
2020-07-18 14:44:38 +0800

    Posts as Cards

--- db/seeds/features.seeds.rb
+++ db/seeds/features.seeds.rb
@@ -1,4 +1,4 @@
-after :channels do
+after :channels, :posts do
   Channel.order("RANDOM()").limit(10).each do |channel|
     Feature.create!(channel: channel, expired_at: Time.now + (rand(10) + 1).days)
 
--- db/seeds/posts.seeds.rb
+++ db/seeds/posts.seeds.rb
@@ -1,19 +1,21 @@
-file = File.read("./data/posts.json")
-posts = JSON.parse(file)
+after :channels do
+  file = File.read("./data/posts.json")
+  posts = JSON.parse(file)
 
-posts.each do |post_channel_data|
-  channel = Channel.find_by(slug: post_channel_data['slug'])
-  
-  post_channel_data["contents"].each do |post_data|
-    post = Post.create(
-      name: post_data["name"],
-      description: post_data["description"],
-      slug: post_data["slug"],
-      published_at: post_data["published_at"],
-      source_url: post_data["sources"][0]["url"],
-      channel: channel
-    )
+  posts.each do |post_channel_data|
+    channel = Channel.find_by(slug: post_channel_data['slug'])
+    
+    post_channel_data["contents"].each do |post_data|
+      post = Post.create(
+        name: post_data["name"],
+        description: post_data["description"],
+        slug: post_data["slug"],
+        published_at: post_data["published_at"],
+        source_url: post_data["sources"][0]["url"],
+        channel: channel
+      )
 
-    puts "Created: #{ post.name } Post"
+      puts "Created: #{ post.name } Post"
+    end
   end
 end
