a29a844577d42052a58ee7cb6e0cb8f620a8c368
Dirk Kelly <github@dirkkelly.com>
2020-07-18 14:56:10 +0800

    Channel Cards

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @features = Feature.all.includes(:channel)
+    @posts = Post.order("RANDOM()").limit(10)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :channel
 
+  def image
+    "https://img.youtube.com/vi/#{ source_id }/hqdefault.jpg"
+  end
+
   def source_id
     source_url.gsub("https://www.youtube.com/watch?v=", "")
   end
