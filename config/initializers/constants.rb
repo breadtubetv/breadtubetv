2cb12859e02a3f786605fd41597de1fee0a7e6a3
Dirk Kelly <github@dirkkelly.com>
2020-07-24 23:33:04 +0800

    footer update

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,9 +1,10 @@
 LINKS = {
-  peertube: 'https://watch.breadtube.tv/',
-  peertube_upload: 'https://watch.breadtube.tv/videos/upload',
-  youtube: 'https://www.youtube.com/channel/UCv8O1dx5UjWHlJuo_Wl6o5w',
-  reddit: 'https://www.reddit.com/r/breadtube',
   discord: 'https://discord.com/invite/ynn9rHE',
   facebook: 'https://www.facebook.com/breadtubetv',
-  twitter: 'https://www.twitter.com/breadtubetv'
+  gitlab: 'https://gitlab.com/breadtube',
+  peertube: 'https://watch.breadtube.tv/',
+  peertube_upload: 'https://watch.breadtube.tv/videos/upload',
+  reddit: 'https://www.reddit.com/r/breadtube',
+  twitter: 'https://www.twitter.com/breadtubetv',
+  youtube: 'https://www.youtube.com/channel/UCv8O1dx5UjWHlJuo_Wl6o5w'
 }
