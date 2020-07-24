3f3b011aef24809758c6a28a111335fa367cadec
Dirk Kelly <github@dirkkelly.com>
2020-07-24 21:06:56 +0800

    Peertube Orange

--- db/seeds/features.seeds.rb
+++ db/seeds/features.seeds.rb
@@ -1,5 +1,10 @@
 after :channels, :videos do
-  [:noncompete, :somemorenews, :prolekult, :themajorityreport, :philosophytube, :thoughtslime, :contrapoints, :angiespeaks, :petercoffin, :unicornriot, :therationalnational, :thejuicemedia].each do |channel_slug|
+  [
+    :radshiba, :indefenseoftoucans, :themichaelbrooksshow,
+    :thejuicemedia, :noncompete, :somemorenews,
+    :prolekult, :angiespeaks, :themajorityreport,
+    :philosophytube, :thoughtslime, :contrapoints
+  ].each do |channel_slug|
     channel = Channel.find_by(slug: channel_slug)
     Feature.create!(channel: channel, expire_at: Time.now + (rand(10) + 1).days)
 
