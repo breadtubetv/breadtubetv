5d7687b4a42144b33a3b2e72e45f78f4fd7a3954
Dirk Kelly <github@dirkkelly.com>
2020-07-28 19:52:23 +0800

    Random Videos on Homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @latest_videos = Video.includes(:channel, :sources, :youtube).published.random.this_week.limit(12)
+    @random_videos = Video.includes(:channel, :sources, :youtube).published.random.limit(12)
     @random_channels = Channel.includes(:sources).random.limit(12)
   end
 end
