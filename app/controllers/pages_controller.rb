653cd9a1ad4b2ae0f4a784120747ed539093bd18
Dirk Kelly <github@dirkkelly.com>
2020-11-14 22:24:53 +0800

    Reorder Latest and Random channels

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
+  ROW_COUNT=12
+
   layout "text", except: [:home]
 
   def home
-    @latest_videos = Video.kept.includes(:channel, :sources, :youtube).published.random.this_week.limit(12)
-    @latest_channels = Channel.kept.includes(:sources).order_by_created.limit(12)
-    @random_videos = Video.kept.includes(:channel, :sources, :youtube).published.random.limit(12)
-    @random_channels = Channel.kept.includes(:sources).random.limit(12)
+    @latest_videos = Video.kept.includes(:channel, :sources, :youtube).published.random.this_week.limit(ROW_COUNT)
+    @latest_channels = Channel.kept.includes(:sources).order_by_created.limit(ROW_COUNT)
+    @random_videos = Video.kept.includes(:channel, :sources, :youtube).published.random.limit(ROW_COUNT)
+    @random_channels = Channel.kept.includes(:sources).random.limit(ROW_COUNT)
   end
 end
